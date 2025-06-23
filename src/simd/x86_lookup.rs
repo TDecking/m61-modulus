628f0427b848a32422c5bca40fc94dff4c1b517a
Tobias Decking <Tobias.Decking@gmail.com>
2025-06-23 20:49:07 +0200

    Fix the custom x86 feature detection machine

--- src/simd/x86_lookup.rs
+++ src/simd/x86_lookup.rs
@@ -11,8 +11,7 @@
 //! target features and overwrites the variable based on the result,
 //! before calling the appropriate implementation.
 //!
-//! This means that any subsequent calls immediately use
-//! the appropriate version.
+//! This means that any subsequent calls immediately use the appropriate version.
 
 /// Obtain information about the available target features by
 /// using the `is_x86_feature_detected` macro provided by
@@ -35,7 +34,6 @@ mod detection {
 /// using the `cpuid` intruction. Used in no-std builds.
 #[cfg(not(feature = "std"))]
 mod detection {
-    //__cpuid_count
     #[cfg(target_arch = "x86")]
     use core::arch::x86::{__cpuid_count as cpuid, _xgetbv};
     #[cfg(target_arch = "x86_64")]
@@ -51,30 +49,40 @@ mod detection {
     static CACHE: AtomicU8 = AtomicU8::new(UNINIT);
 
     fn get_features() -> u8 {
+        if cfg!(not(target_feature = "sse2")) {
+            return 0;
+        }
+
         let content = CACHE.load(Ordering::Relaxed);
         if content == UNINIT {
             let mut content = SSE2;
 
             // SAFETY: This will only run on systems with support
-            // SSE2, which in turn implies support for CPUID.
+            // for SSE2, which in turn implies support for CPUID.
             unsafe {
-                // Testing for AVX2 or AVX512 requires the following
-                // 1. Support for XSAVE by the hardware.
-                // 2. Support for XSAVE by the operating system.
-                // 3. Registers are enabled when checking XCR0.
-                // 4. Support for the instructions.
+                // A precondition for AVX2 or AVX512 support is
+                // the presense of the XSAVE instruction _and_ its
+                // use by the operating system. The former can be checked
+                // by testing bit 26 of the ecx output of CPUID leaf 1, the
+                // latter by testing bit 27 of the same output.
                 if cpuid(0x01, 0x0).ecx & (0b11 << 26) != 0 {
-                    // Query the extended control register.
+                    // Query the extended control register. It contains information
+                    // about which registers are getting saved. If bit 1 is set then SSE
+                    // registers are getting saved. Bit 2 indicates the same thing about
+                    // the upper halves introduced with AVX. Bits 5, 6, and 7 are related
+                    // to the AVX512 registers.
                     let xcr0 = _xgetbv(0);
-                    // Query extended features.
+
+                    // Query extended feature flags. Bit 5 indicates support
+                    // for AVX2, while bit 16 indicates support for AVX512F.
                     let ebx = cpuid(0x07, 0x0).ebx;
 
-                    // Support for AVX2
+                    // Check support for AVX2.
                     if xcr0 & 6 == 6 && ebx & (1 << 5) != 0 {
                         content = AVX2;
                     }
 
-                    // Support for AVX512F
+                    // Check support for AVX512F.
                     if xcr0 & 224 == 224 && ebx & (1 << 16) != 0 {
                         content = AVX512;
                     }
@@ -113,22 +121,21 @@ use core::sync::atomic::{AtomicPtr, Ordering};
 
 use crate::definition::M61;
 
-// These variables contain fuction pointers to the impementations.
+// These variables contain the function pointers to the impementations.
 
 static FUNC8: AtomicPtr<()> = AtomicPtr::new(reduce_u8_init as *mut ());
 static FUNC16: AtomicPtr<()> = AtomicPtr::new(reduce_u16_init as *mut ());
 static FUNC32: AtomicPtr<()> = AtomicPtr::new(reduce_u32_init as *mut ());
 static FUNC64: AtomicPtr<()> = AtomicPtr::new(reduce_u64_init as *mut ());
 
-/// Writes the appropiate versions of the functions into the
-/// static variables.
+/// Writes the appropiate versions of the functions into the static variables.
 unsafe fn select() {
-    #[cfg(feature = "nightly")]
-    if has_avx512f() {
-        FUNC8.store(avx512::reduce_u8 as *mut (), Ordering::Relaxed);
-        FUNC16.store(avx512::reduce_u16 as *mut (), Ordering::Relaxed);
-        FUNC32.store(avx512::reduce_u32 as *mut (), Ordering::Relaxed);
-        FUNC64.store(avx512::reduce_u64 as *mut (), Ordering::Relaxed);
+    #[cfg(not(target_feature = "avx2"))]
+    {
+        FUNC8.store(sse2::reduce_u8 as *mut (), Ordering::Relaxed);
+        FUNC16.store(sse2::reduce_u16 as *mut (), Ordering::Relaxed);
+        FUNC32.store(sse2::reduce_u32 as *mut (), Ordering::Relaxed);
+        FUNC64.store(sse2::reduce_u64 as *mut (), Ordering::Relaxed);
     }
 
     if has_avx2() {
@@ -138,66 +145,39 @@ unsafe fn select() {
         FUNC64.store(avx2::reduce_u64 as *mut (), Ordering::Relaxed);
     }
 
-    #[cfg(not(target_feature = "avx2"))]
-    {
-        FUNC8.store(sse2::reduce_u8 as *mut (), Ordering::Relaxed);
-        FUNC16.store(sse2::reduce_u16 as *mut (), Ordering::Relaxed);
-        FUNC32.store(sse2::reduce_u32 as *mut (), Ordering::Relaxed);
-        FUNC64.store(sse2::reduce_u64 as *mut (), Ordering::Relaxed);
+    #[cfg(feature = "nightly")]
+    if has_avx512f() {
+        FUNC8.store(avx512::reduce_u8 as *mut (), Ordering::Relaxed);
+        FUNC16.store(avx512::reduce_u16 as *mut (), Ordering::Relaxed);
+        FUNC32.store(avx512::reduce_u32 as *mut (), Ordering::Relaxed);
+        FUNC64.store(avx512::reduce_u64 as *mut (), Ordering::Relaxed);
     }
 }
 
-// Helper types used to keep calls to `transmute` clean.
+/// A helper macro for creating the implementations.
+macro_rules! make_implementation {
+    ($name:ident, $init_name:ident, $atomic_ptr:ident, $type:ty) => {
+        // Used as a starting value for the static variable.
+        unsafe fn $init_name(s: &[$type]) -> M61 {
+            select();
+            let func = transmute::<*mut (), unsafe fn(&[$type]) -> M61>(
+                $atomic_ptr.load(Ordering::Relaxed),
+            );
+            func(s)
+        }
 
-type T8 = unsafe fn(&[u8]) -> M61;
-type T16 = unsafe fn(&[u16]) -> M61;
-type T32 = unsafe fn(&[u32]) -> M61;
-type T64 = unsafe fn(&[u64]) -> M61;
-
-// Definition of the starting values of the static variables.
-
-unsafe fn reduce_u8_init(s: &[u8]) -> M61 {
-    select();
-    let func = transmute::<_, T8>(FUNC8.load(Ordering::Relaxed));
-    func(s)
+        // Definition of the exported function.
+        #[inline]
+        pub(crate) unsafe fn $name(s: &[$type]) -> M61 {
+            let func = transmute::<*mut (), unsafe fn(&[$type]) -> M61>(
+                $atomic_ptr.load(Ordering::Relaxed),
+            );
+            func(s)
+        }
+    };
 }
 
-unsafe fn reduce_u16_init(s: &[u16]) -> M61 {
-    select();
-    let func = transmute::<_, T16>(FUNC16.load(Ordering::Relaxed));
-    func(s)
-}
-
-unsafe fn reduce_u32_init(s: &[u32]) -> M61 {
-    select();
-    let func = transmute::<_, T32>(FUNC32.load(Ordering::Relaxed));
-    func(s)
-}
-
-unsafe fn reduce_u64_init(s: &[u64]) -> M61 {
-    select();
-    let func = transmute::<_, T64>(FUNC64.load(Ordering::Relaxed));
-    func(s)
-}
-
-// Definition of the exports.
-
-pub unsafe fn reduce_u8(s: &[u8]) -> M61 {
-    let func = transmute::<_, T8>(FUNC8.load(Ordering::Relaxed));
-    func(s)
-}
-
-pub unsafe fn reduce_u16(s: &[u16]) -> M61 {
-    let func = transmute::<_, T16>(FUNC16.load(Ordering::Relaxed));
-    func(s)
-}
-
-pub unsafe fn reduce_u32(s: &[u32]) -> M61 {
-    let func = transmute::<_, T32>(FUNC32.load(Ordering::Relaxed));
-    func(s)
-}
-
-pub unsafe fn reduce_u64(s: &[u64]) -> M61 {
-    let func = transmute::<_, T64>(FUNC64.load(Ordering::Relaxed));
-    func(s)
-}
+make_implementation!(reduce_u8, reduce_u8_init, FUNC8, u8);
+make_implementation!(reduce_u16, reduce_u16_init, FUNC16, u16);
+make_implementation!(reduce_u32, reduce_u32_init, FUNC32, u32);
+make_implementation!(reduce_u64, reduce_u64_init, FUNC64, u64);
